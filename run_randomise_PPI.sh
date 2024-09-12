402bd2ed4ef4127ce8ae7616bbe9a0f880d39cb0
Valeria Oliva <50948442+Vale-90@users.noreply.github.com>
2024-09-12 11:24:04 +0200

    added comment

--- run_randomise_PPI.sh
+++ run_randomise_PPI.sh
@@ -1,5 +1,7 @@
-
 #!/bin/bash
+
+# Run randomise for all masks with 3mm variance smoothing and 10000 permutations
+
 echo -n "Seed region?"
 read region
 echo -n "Input folder?"
@@ -13,9 +15,10 @@ for j in cope10 cope8 cope12; do ## cope8 is PPI main effect of task, cope10 is 
   inpath=${folder}/${j}.feat ;
   maskpath=/home/vo16427/GROUPMASKS ;
   echo $inpath ; 
-  for k in $maskpath/fsl5*; do 
-    mask=$(basename $k .nii.gz) ; 
-    echo "Working on mask ${mask}" ;
+  for k in $maskpath/*.nii.gz
+    do 
+    mask=$(basename $k .nii.gz) 
+    echo "Working on mask ${mask}"
     $FSLDIR/bin/randomise_parallel -i $inpath/filtered_func_data -o $outpath/$j/$mask -m $k -1 -T -x -n 10000 -v 3; 
-    done ; 
+    done  
 done
