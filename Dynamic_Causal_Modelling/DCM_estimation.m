26162f6155ed33843b5087f0542479dd36313938
Valeria Oliva <50948442+Vale-90@users.noreply.github.com>
2024-09-12 11:29:09 +0200

    added comment

--- Dynamic_Causal_Modelling/DCM_estimation.m
+++ Dynamic_Causal_Modelling/DCM_estimation.m
@@ -1,1 +1,3 @@
-addpath(genpath('/panfs/panasas01/phph/vo16427/spm12')); clear DCM; load '~/Data_for_DCM/002/DCM/DCM_2inputs_noB'; spm_dcm_estimate(DCM);
+% Example matlab script used to estimate each DCM for each subject individually 
+
+addpath(genpath('/panfs/panasas01/phph/vo16427/spm12')); clear DCM; load '~/Data_for_DCM/002/DCM/DCM_example'; spm_dcm_estimate(DCM);
