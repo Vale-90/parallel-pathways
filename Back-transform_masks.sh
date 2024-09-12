efdbbef5418c0d860c7ee903da102ae5a4fd49c8
Valeria Oliva <50948442+Vale-90@users.noreply.github.com>
2024-09-12 11:27:29 +0200

    added comment and made minor mods to Back-transform_masks.sh

--- Back-transform_masks.sh
+++ Back-transform_masks.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-basepath=/home/vo16427/Data_FM
+# Script used to warp masks from standard MNI space back to subject space
+
+basepath=/home/vo16427/Data
 maskpath=/home/vo16427/GROUPMASKS
 
 ( cd $basepath ;
-for i in FM2? FM3? FM40 ; do
+for i in 0?? ; do
 
-	outpath=$basepath/$i
-	datapath=$basepath/$i/tests/all_corrections_no_ST_fsl5.feat
+	outpath=$basepath/${i}
+	datapath=$basepath/${i}/tests/all_corrections_no_ST_fsl5.feat
 	regpath=$datapath/reg
-	echo 'working on='$i
+	echo 'working on='${i}
 
 	for m in $maskpath/fsl5_* ; do
 		mask=$(basename $m .nii.gz)
 		echo 'working on='${mask}' mask'
-		fsl_sub $FSLDIR/bin/applywarp -i $m -o /home/vo16427/Data_FM/$i/${mask}for${i}_all_nostc -r $regpath/example_func -w $regpath/standard2highres_warp.nii.gz --postmat=$regpath/highres2example_func.mat --interp=nn ;
+		fsl_sub $FSLDIR/bin/applywarp -i $m -o /home/vo16427/Data/$i/${mask}for${i}_all_nostc -r $regpath/example_func -w $regpath/standard2highres_warp.nii.gz --postmat=$regpath/highres2example_func.mat --interp=nn ;
 		
 	done
 done
